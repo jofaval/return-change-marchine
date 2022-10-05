143a8dc98c495ba99f2f17627bc0de1a4e35de7c
pepe_fabra_valverde <jofaval@outlook.com>
2022-10-05 15:22:07 +0200

    test: Add too much test

--- logic_test.go
+++ logic_test.go
@@ -31,3 +31,23 @@ func TestGetChangeReturnNotEnoughAmount(t *testing.T) {
 		t.Error("Expected to not have enough")
 	}
 }
+
+func TestGetChangeReturnHasTooMuch(t *testing.T) {
+	got := GetChangeReturn(300, []ChangeType{
+		{
+			name:   "500.EUR",
+			value:  1,
+			units:  EUROS,
+			amount: 1,
+		},
+		{
+			name:   "3.EUR",
+			value:  3,
+			units:  EUROS,
+			amount: 1,
+		},
+	})
+	if len(got) != 1 {
+		t.Error("Expected to not have enough")
+	}
+}
